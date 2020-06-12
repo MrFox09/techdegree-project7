ba98a53e539f4181e73838d76014d72344f05688
Christian Fuchs <christian.fuchs5@gmx.at>
2020-06-12 14:10:27 +0200

    update
    added routes

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './index.css';
+import apiKey from './config';
+import {BrowserRouter, Route, Switch,Redirect} from 'react-router-dom';
+
 
 // import Components
 
 import Search from './components/Search';
 import Nav from './components/Nav';
+import PhotoContainer from './components/PhotoContainer';
+import NotFound from './components/NotFound';
 
 
 
-function App() {
-  return (
-    <div >
+class App extends Component {
 
-    <Search />
-    <Nav />
-      
-        
-    </div>
-  );
+
+
+  render(){
+    return (
+      <BrowserRouter>
+        <div className= 'container' >
+    
+          <Search />
+          <Nav />
+          <Switch>
+          <Route exact path="/" render ={() =><Redirect to='/cats'/> } />
+            <Route exact path="/cats" render= {() => <PhotoContainer />  }  />
+            <Route exact path="/dogs" render= {() => <PhotoContainer />  }  />
+            <Route exact path="/computers" render= {() => <PhotoContainer />  }  />
+            <Route component={NotFound} />
+          </Switch>
+            
+        </div>
+      </BrowserRouter>
+    );
+
+  }
+  
+  
 }
 
 export default App;
