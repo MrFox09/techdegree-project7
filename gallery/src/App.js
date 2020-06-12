c15d926d5e1b533edca86f265433866147dc9219
Christian Fuchs <christian.fuchs5@gmx.at>
2020-06-12 12:20:47 +0200

    update
    added components

---
+++ gallery/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+// import Components
+
+import Search from './components/Search';
+import Nav from './components/Nav';
+
+
+
+function App() {
+  return (
+    <div >
+
+    <Search />
+    <Nav />
+      
+        
+    </div>
+  );
+}
+
+export default App;
