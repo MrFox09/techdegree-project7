923278d3bdccdf160a2e4d58e02b3846e0d82154
Christian Fuchs <christian.fuchs5@gmx.at>
2021-02-05 12:28:39 +0100

    create build

---
+++ build/precache-manifest.5026883056e9872febc824d2a04be2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de86e8564657c5bd5fe7583b57d6258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "307a71f87cb42d734afd",
+    "url": "/static/css/main.8325ea2a.chunk.css"
+  },
+  {
+    "revision": "b49bb6b737dc72d03125",
+    "url": "/static/js/2.ea9a457a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea9a457a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307a71f87cb42d734afd",
+    "url": "/static/js/main.46f2530b.chunk.js"
+  },
+  {
+    "revision": "58382bf7dc2eb24d69d2",
+    "url": "/static/js/runtime-main.ed2e08ff.js"
+  }
+]);
