541befb7e617d125a9536fce24259207924cf34b
Christian Fuchs <christian.fuchs5@gmx.at>
2021-02-05 11:43:15 +0100

    update

--- expressServer.js
+++ expressServer.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'gallery/build');
+const publicPath = path.join(__dirname, '..', 'techdegree-project7/build');
 
 const port = process.env.PORT || 3000;
 
