280c9ff9a6d46b4120b1d62e35c0dbb042e34f8f
Christian Fuchs <christian.fuchs5@gmx.at>
2021-02-05 11:57:28 +0100

    Update expressServer.js

--- expressServer.js
+++ expressServer.js
@@ -1,22 +1,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'techdegree-project7/build');
+
 
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(publicPath));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-
-//serve the index file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
  // listen to the given port or port 3000 by default
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
+    
     
  });
